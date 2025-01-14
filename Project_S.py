6a0958720a290eee64bf11ddb6b7146bb77c9222
SHAILESH S <100753992+shaileshgoku@users.noreply.github.com>
2025-01-14 13:13:29 +0530

    Add files via upload

---
+++ Project_S.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas as pd
+
+
+st.title(":rainbow[Happy Birthday My Dear Queen]")
+st.title(":heart::heart::heart::heart::heart::heart::heart::heart::heart::heart:")
+
+st.title("This song is for you")
+
+st.audio("project.mp3")
