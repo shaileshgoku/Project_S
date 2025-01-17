63f59eb145269ae4d7b45447f2569eefbf8c2a43
SHAILESH S <100753992+shaileshgoku@users.noreply.github.com>
2025-01-17 20:27:03 +0530

    Update Project_S.py
    Added the line as "dear chocolate girl"

--- Project_S.py
+++ Project_S.py
@@ -5,6 +5,6 @@ import pandas as pd
 st.title(":rainbow[Happy Birthday My Dear Queen]")
 st.title(":heart::heart::heart::heart::heart::heart::heart::heart::heart::heart:")
 
-st.title("This song is for you")
+st.title("This song is for you dear chocolate girl")
 
 st.audio("project.mp3")
