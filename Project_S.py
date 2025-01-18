60222f3f7297d040a4b0063dc71ce65f68a3a6d8
SHAILESH S <100753992+shaileshgoku@users.noreply.github.com>
2025-01-18 21:29:32 +0530

    Update Project_S.py
    Added extra two hearts and removed library pandas

--- Project_S.py
+++ Project_S.py
@@ -1,9 +1,8 @@
 import streamlit as st
-import pandas as pd
 
 
 st.title(":rainbow[Happy Birthday My Dear Queen]")
-st.title(":heart::heart::heart::heart::heart::heart::heart::heart::heart::heart:")
+st.title(":heart::heart::heart::heart::heart::heart::heart::heart::heart::heart::heart::heart:")
 
 st.title("This song is for you dear chocolate girl")
 
